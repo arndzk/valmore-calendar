5038768d772a26b3fcecc02f947aa5f955774744
arndzk <22920826+arndzk@users.noreply.github.com>
2021-02-10 20:29:38 +0200

    progress: AddEvent controlled form with Hooks

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,10 +1,44 @@
-import React from 'react';
+import React, { useReducer } from 'react';
 import '../styles/Form.css';
 
 const Form = (props) => {
+  const initialState = {
+    eventName: '',
+    eventDescription: '',
+  };
+
+  const reducer = (state, { property, value }) => {
+    return {
+      ...state,
+      [property]: value,
+    };
+  };
+
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  const handleChange = (event) => {
+    dispatch({ property: event.target.name, value: event.target.value });
+  };
+
+  const { eventName, eventDescription } = state;
+
   return (
     <div className="form-container">
-      <div className="form">
+      <form className="form">
+        <div className="form-inputs">
+          <input
+            name="eventName"
+            value={eventName}
+            onChange={handleChange}
+            placeholder="Event Name"
+          />
+          <input
+            name="eventDescription"
+            value={eventDescription}
+            onChange={handleChange}
+            placeholder="Event Description"
+          />
+        </div>
         <div className="form-btns">
           <div className="save-event btn">
             <span>Save Event</span>
@@ -13,7 +47,7 @@ const Form = (props) => {
             <span>Cancel</span>
           </div>
         </div>
-      </div>
+      </form>
     </div>
   );
 };
